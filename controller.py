44816e4304f0cc0997734ff2ad9dcb01c3deab0b
John Dopping <john.dh@hotmail.co.uk>
2026-08-01 18:32:37 +0100

    Added interpolation slope for RPM

--- controller.py
+++ controller.py
@@ -15,9 +15,30 @@ class Controller:
     def find_curve_segment(self, temperature):
         lower = None
         upper = None
+
         for temp, rpm in self.curve:
-            if temperature < temp:
+            if temperature <= temp:
                 upper = (temp, rpm)
                 break
             lower = (temp, rpm)
+
         return lower, upper
+
+    def get_interpolated_rpm(self, temperature):
+        lower, upper = self.find_curve_segment(temperature)
+
+        if lower is None:
+            return upper[1]
+        if upper is None:
+            return lower[1]
+
+        lower_temp, lower_rpm = lower
+        upper_temp, upper_rpm = upper
+
+        # Linear interpolation formula
+        slope = (upper_rpm - lower_rpm) / (upper_temp - lower_temp)
+        interpolated_rpm = lower_rpm + slope * (temperature - lower_temp)
+
+        return int(interpolated_rpm)
+   
+
