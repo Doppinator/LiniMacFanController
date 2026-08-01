9983c5724cef8db4d3ae2d73e9d1a029fa475dd1
John Dopping <john.dh@hotmail.co.uk>
2026-08-01 15:44:36 +0100

    Main prints curve values to test class access

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from fan import Fan
 from smc import SMC
+from controller import Controller
 
 
 def main():
 
     smc = SMC()
     fans = [Fan(number, smc) for number in smc.fan_numbers()]
+    controller = Controller()
 
-    print()
-
+    print("Fan control curve:")
+    for temperature, rpm in controller.curve:
+        print(f"  {temperature}°C -> {rpm} RPM")
+        
     if fans:
         for fan in fans:
             print(fan)
     else:
         print("No fans were reported by the Apple SMC driver.")
-
-    print()
-
 
 if __name__ == "__main__":
     main()
