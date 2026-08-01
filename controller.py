722f500958142eca7231c532d377eaa7cf77463c
John Dopping <john.dh@hotmail.co.uk>
2026-08-01 16:35:41 +0100

    Added curve segment loop

--- controller.py
+++ controller.py
@@ -11,3 +11,13 @@ class Controller:
             if temperature <= temp:
                 return rpm
         return self.curve[-1][1]
+
+    def find_curve_segment(self, temperature):
+        lower = None
+        upper = None
+        for temp, rpm in self.curve:
+            if temperature < temp:
+                upper = (temp, rpm)
+                break
+            lower = (temp, rpm)
+        return lower, upper
