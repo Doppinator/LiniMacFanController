e346260f77f24653bc13bd6181377810f6d1e3a3
John Dopping <john.dh@hotmail.co.uk>
2026-08-02 09:10:17 +0100

    Created sensor collection

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def main():
     smc = SMC()
     fans = [Fan(number, smc) for number in smc.fan_numbers()]
     controller = Controller()
-    sensor = Sensor(1, smc)  # Assuming sensor number 1 is the CPU temperature sensor
+    sensors = [Sensor(number, smc) for number in smc.sensor_numbers()]  
 
     while True:
-        for sensor in [sensor]:
+        for sensor in sensors:
             temperature = sensor.celsius
             rpm = controller.get_interpolated_rpm(temperature)
             print(f"sensor {sensor.number}: {temperature:.1f}°C -> {rpm} RPM")
