e04bdd62fe8f5af0724b3e38b5d76c9a86f43eb2
John Dopping <john.dh@hotmail.co.uk>
2026-08-03 12:46:11 +0100

    Removed duplications in main loop

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import time
 
-from LiniMacFanController import explorer
-from LiniMacFanController.controller import Controller
-from LiniMacFanController.explorer.fans import Fan
-from LiniMacFanController.explorer.sensors import Sensor
 from LiniMacFanController.explorer import HardwareExplorer
-from LiniMacFanController.smc import SMC
 
 def main():
-    smc = SMC()
-    fans = [Fan(number, smc) for number in smc.fan_numbers()]
-    controller = Controller()
-    sensors = [Sensor(number, smc) for number in smc.sensor_numbers()]
     explorer = HardwareExplorer()
+
+    explorer.discover()
+
     while True:
-        explorer.discover()
         explorer.refresh()
+        print(explorer)
         time.sleep(5)
 
 if __name__ == "__main__":
