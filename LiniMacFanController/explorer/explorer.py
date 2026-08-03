3aedb13104187d4a0d1c81ba990b372c69567c5a
John Dopping <john.dh@hotmail.co.uk>
2026-08-03 09:53:59 +0100

    Added HW explore scaffold

--- LiniMacFanController/explorer/explorer.py
+++ LiniMacFanController/explorer/explorer.py
@@ -1,11 +1,15 @@
-class Explorer:
+class HardwareExplorer:
 
     def __init__(self):
+        self.smc = SMC()
         self.fans = []
         self.sensors = []
-
     def refresh(self):
         ...
 
     def __str__(self):
         ...
+
+    def discover(self):
+        self.refresh()
+        return self
