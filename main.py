b4cf0cc35098ed12964951f0899457f0c40f50de
John Dopping <john.dh@hotmail.co.uk>
2026-08-12 03:32:58 +0100

    Added terminal clearing per refresh

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import time
+import os
 
 from LiniMacFanController.explorer import HardwareExplorer
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
     explorer = HardwareExplorer()
@@ -9,8 +13,12 @@ def main():
 
     while True:
         explorer.refresh()
+
+        clear_terminal()
+
         print(explorer)
-        time.sleep(5)
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
